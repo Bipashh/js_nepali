da929913ab89763d89720337145983ecaf687e3a
Bipashh <114556150+Bipashh@users.noreply.github.com>
2024-05-23 07:15:53 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+let accountName = "Bipash"
+//var accound (Prefer not to use var because of issue in block and functional scope)
+const accountId = 1
+//console.log(accountName);
+//accountId = 2
+accountName = "Roshni"
+console.log(accountName);
+console.table([accountName, accountId])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat js code as newer version
+
+//alert( 3 + 3) //We are using node js not browser
+
+let name = "Bipash"
+
+let age = 12
+let isloggedIn = false
+let state;
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+// object
+
+console.log(typeof(age));
