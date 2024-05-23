7ba84cc7545a0302b427830cb1995cd41e87a26b
Bipashh <114556150+Bipashh@users.noreply.github.com>
2024-05-23 07:45:58 +0000

    Type casting datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+
+//Type casting
+// "33" => 33
+//"33abc" => NaN(not a number)
+// true => 1; false => 0;
+
+let isloggedIn = "Bipash"
+
+let BooleanIsLoggedIn = Boolean(isloggedIn)
+console.log(BooleanIsLoggedIn);
+
+// 1 -> true; 0 -> false
+//"" => false
+//"Bipash" => true
