9e4cf45c1137704133e56b49dcd802b96a13875a
Bipashh <114556150+Bipashh@users.noreply.github.com>
2024-05-24 07:01:09 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,15 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof(myDate));
+
+//let myCreatedDate = new Date(2023, 0, 23)
+let myCreatedDate = new Date("2023-01-14")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimestamp = Date.now()
+
+console.log(myTimestamp);
