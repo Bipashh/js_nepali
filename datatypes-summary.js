00b210ddfa2512badd52aae8da9623f379e54d84
Bipashh <114556150+Bipashh@users.noreply.github.com>
2024-05-24 02:43:09 +0000

    datatype summary

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -2,6 +2,32 @@
 
 // 7 types : String, Number, Boolean , null, undefined, symbol, BigInt
 
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 998178268761798172n
+
 //Reference (Non Primitive)
 
 // Array, Object, functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name:"Bipash",
+    age:20,
+}
+
+const myfunction = function() {
+    console.log("Hello Workd");
+}
+
