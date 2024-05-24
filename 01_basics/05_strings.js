9f0f59ef1dbf94ee94a2f2f112ba54a70637dacd
Bipashh <114556150+Bipashh@users.noreply.github.com>
2024-05-24 03:22:23 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "Bipash"
+const RepoCount = 10
+
+// console.log(name + RepoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${RepoCount}`);
+
+const gameName = new String("BipashG");
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
