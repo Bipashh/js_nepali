d3d0c18b0ba9284e2bcdcc030214314ef62e238d
Bipashh <114556150+Bipashh@users.noreply.github.com>
2024-05-26 13:26:39 +0000

    fucations

---
+++ 03_basics/01_fuctions.js
@@ -0,0 +1,32 @@
+
+// function saymyname(){
+//     console.log("B");
+//     console.log("I");
+//     console.log("P");
+//     console.log("A");
+//     console.log("S");
+//     console.log("H");
+// }
+
+// saymyname()
+
+// function addtwoNumber(num1, num2){//parameters
+//     console.log(num1 + num2);
+// }
+
+function addtwoNumber(num1, num2){
+    // let result = num1+ num2
+    // return result
+    return num1 + num2
+
+}
+
+const result = addtwoNumber(3,4)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Bipash"))
