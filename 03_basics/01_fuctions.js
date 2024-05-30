25e1985bc210451df37530807a376d51ce7bfa81
Bipashh <114556150+Bipashh@users.noreply.github.com>
2024-05-30 03:46:14 +0000

    functions

--- 03_basics/01_fuctions.js
+++ 03_basics/01_fuctions.js
@@ -1,32 +1,63 @@
-
-// function saymyname(){
-//     console.log("B");
-//     console.log("I");
-//     console.log("P");
-//     console.log("A");
-//     console.log("S");
-//     console.log("H");
-// }
-
-// saymyname()
-
-// function addtwoNumber(num1, num2){//parameters
-//     console.log(num1 + num2);
-// }
-
-function addtwoNumber(num1, num2){
-    // let result = num1+ num2
-    // return result
-    return num1 + num2
-
+function Myfunction() {
+    console.log("B")
+    console.log("I")
+    console.log("P")
+    console.log("A")
+    console.log("S")
+    console.log("H")
 }
+// Myfunction()
 
-const result = addtwoNumber(3,4)
+// function sum(a,b) {
+//    console.log(a+b)
+// }
+
+function sum(a,b) {
+    // let result = a + b
+    // return result
+    return a + b
+    console.log("Bipash");//after result nothing will work
+ }
+const result = sum(3,4)
 
 // console.log("Result: ", result);
 
-function loginUserMessage(username){
+function loginUserMessage(username = "bps"){
+    if(!username){
+        console.log("please enter a username")
+        return
+    }
     return `${username} just logged in`
+
+}
+// console.log(loginUserMessage("Bipash"))
+console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1){// spread and rest both but depent how its being used
+    console.log(val1, val2);
+    return num1
 }
 
-console.log(loginUserMessage("Bipash"))
+console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    name : "Bipash",
+    prices : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    name: "Bipu",
+    price: 299
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500]));
